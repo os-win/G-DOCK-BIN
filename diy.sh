1f5f57151bd8da3a911a5aca67026e88a0957968
os-win <13250562193@qq.com>
2020-02-29 23:18:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+echo '下载ServerChan'
+git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
